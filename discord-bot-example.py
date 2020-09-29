ec724431ee92b93fbb272c15d0016fb99e43860c
bumbelbee777 <64847808+bumbelbee777@users.noreply.github.com>
2020-09-29 09:28:23 +0100

    Update discord-bot-example.py

--- discord-bot-example.py
+++ discord-bot-example.py
@@ -38,5 +38,13 @@ async def userinfo(ctx, member: discord.Member = None):
         messageok = f"You have been banned from {ctx.guild.name} for {reason}"
         await member.send(messageok)
         await member.ban(reason=reason)
-
+  @bot.command(pass_context = True)
+async def mute(ctx, user_id, userName: discord.User):
+    if ctx.message.author.server_permissions.administrator:
+        user = ctx.message.author
+        role = discord.utils.get(user.server.roles, name="Muted")
+        await client.add_roles(user, role)
+     else:
+       embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+       await bot.say(embed=embed)
 @client.run("token")
